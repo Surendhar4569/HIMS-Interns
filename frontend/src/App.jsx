deccb6ccac93b8fd6b3c9ac16e9bb04ed6d1200b
JatinReddyAtluri <atlurijatinreddy@gmail.com>
2026-02-24 10:25:27 +0530

    added route to complaint form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { Box, Flex } from "@chakra-ui/react";
 import { Routes, Route } from "react-router-dom";
-import PatientComplaints from "./pages/patient_complaints";
+
 import Sidebar from "./components/sidebar";
 import Home from "./pages/home";
 import PatientFeedback from "./pages/patient_feedback";
-import AddComplaint from "./pages/Complaint";
+import PatientComplaints from "./pages/patient_complaints";
+
 function App() {
   return (
     <Flex>
@@ -17,12 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/feedback" element={<PatientFeedback />} />
-<<<<<<< HEAD
           <Route path="/complaints" element={<PatientComplaints />} />
-=======
-          <Route path="/complaints" element={<AddComplaint />} />
-
->>>>>>> 63e5d55531d1552a9e7670484d7b8053d98d128a
         </Routes>
       </Box>
 
